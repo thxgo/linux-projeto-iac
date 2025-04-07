f4a905801e7c301e834049b46136c0601df005f9
thxgo <paduthiago02@gmail.com>
2025-04-07 04:00:12 +0000

    Arquivo IaC v2

--- iac1.sh
+++ iac1.sh
@@ -3,16 +3,9 @@
 echo "Criando diretórios..."
 
 sudo mkdir /publico
-chmod 777 /publico
-
 sudo mkdir /adm
-chmod 770 /adm
-
 sudo mkdir /ven
-chmod 770 /ven
-
 sudo mkdir /sec
-chmod 770 /sec
 
 echo "Criando grupos de usuários..."
 
@@ -23,7 +16,7 @@ groupadd GRP_SEC
 echo "Criando usuários..."
 
 useradd carlos -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM 
+useradd maria -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
 useradd joao -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_ADM
 
 useradd debora -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_VEN
@@ -35,6 +28,11 @@ useradd amanda -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd -6 Senha123) -G GRP_SEC
 
 echo "Específicando permissões dos diretórios..."
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
