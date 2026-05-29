2f11b40287949b096c0aecc4228d90fdf5fb3336
thxgo <paduthiago02@gmail.com>
2026-05-29 00:29:30 -0300

    refactor: localize script to english

---
+++ provision_users.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -euo pipefail
+    
+if [[ $EUID -ne 0 ]]; then
+    echo "ERROR: this script must be run as root (sudo)." >&2
+    exit 1
+fi
+
+echo "Creating folders..."
+
+sudo mkdir /public
+sudo mkdir /adm
+sudo mkdir /sales
+sudo mkdir /sec
+
+echo "Creating user groups..."
+
+groupadd GRP_ADM
+groupadd GRP_SALES
+groupadd GRP_SEC
+
+echo "Creating users..."
+
+read -r -s -p "Set the default password to all users: " PASSWORD
+echo
+HASHED_PASSWORD=$(openssl passwd -6 "$PASSWORD")
+unset PASSWORD
+
+useradd carl -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_ADM
+useradd mary -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_ADM
+useradd john -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_ADM
+
+useradd diana -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_SALES
+useradd sebastian -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_SALES
+useradd robert -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_SALES
+
+useradd joe -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_SEC
+useradd amanda -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_SEC
+useradd roger -m -s /bin/bash -p "$HASHED_PASSWORD" -G GRP_SEC
+
+echo "Setting permissions..."
+
+chmod 777 /public
+chmod 770 /adm
+chmod 770 /sales
+chmod 770 /sec
+
+chown root:GRP_ADM /adm
+chown root:GRP_SALES /sales
+chown root:GRP_SEC /sec
+
+echo "Done."
+
